a7f089f4e264f8ec6a993054aef0268b90ef06dc
davidveith0 <davidveith00@gmial.com>
2025-12-10 18:57:28 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://21114.0001112.xyz:28003".to_owned()
 }
 
 #[inline]
