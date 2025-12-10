1c245476bf856b3d1df1a66d4381900160910730
davidveith0 <davidveith00@gmial.com>
2025-12-11 00:35:50 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://21114.0001112.xyz:28003".to_owned()
+    "https://21114.0001112.xyz:21114".to_owned()
 }
 
 #[inline]
